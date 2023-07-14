f15e91612dd8b2d71450c70934607fbfed27978e
shobhitsaini <shobhit.saini@nagarro.com>
2023-07-14 11:59:15 +0530

    first commit

---
+++ src/main/java/com/javatechie/azure/SpringbootAzuresqlApplication.java
@@ -0,0 +1,40 @@
+package com.javatechie.azure;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+public class SpringbootAzuresqlApplication {
+
+    @Autowired
+    private EmployeeRepository repository;
+
+    @PostMapping("/employee")
+    public Employee addEmployee(@RequestBody Employee employee) {
+        return repository.save(employee);
+    }
+
+    @GetMapping("/employees")
+    public List<Employee> getEmployees() {
+        return repository.findAll();
+    }
+    
+    @GetMapping(path="/test")
+    public String test() {
+        return "Application is working file and Azure database is connected successfully";
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootAzuresqlApplication.class, args);
+    }
+
+}
